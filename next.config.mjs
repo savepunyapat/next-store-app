bdbe6ffd1a24088a4772a48bcdf27c97877711f8
Punyapat Lertsamrong <punyapat1412@gmail.com>
2024-06-20 18:25:08 +0700

    decontructed for product page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    modularizeImports: {
+        "@mui/icons-material": {
+          transform: "@mui/icons-material/{{member}}",
+        },
+      },
+    
+      images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'itgenius.co.th, www.abc.co.th',
+            pathname: '**',
+          },
+          {
+            protocol: "http",
+            hostname: "*",
+            pathname: "**",
+          },
+          {
+            protocol: "http",
+            hostname: "localhost",
+            pathname: "**",
+          },
+        ],
+        minimumCacheTTL: 0, // 0 second
+      }
+};
 
 export default nextConfig;
